3d1bfc15df8e924f4829069125eb9ead9058aa68
NavindaHewawickrama <hewawickraman@gmail.com>
2025-01-14 17:41:41 +0530

    top home page done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 //import logo from 'C:/Users/hewaw/OneDrive/Desktop/Portfolio/my-portfolio/src/assets/img/logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './home';
 
 
 function App() {
   return (
     <div className="App">
-    
+      <Home/>
     </div>
   );
 }
--- src/home.jsx
+++ src/home.jsx
@@ -1,14 +1,60 @@
 import React from 'react';
+import './home.css';
+import { Tab } from 'react-bootstrap';
+import imageOne from './assets/img/AM2_0259.jpg'
+import modeIcon from './assets/icons/night-day.svg'
 
 
 
-const Home = () =>{
+const Home = () => {
 
-return(
-    <div>
+    return (
+        <div className='body'>
+            {/* black left side */}
+            <div className='leftColumn'>
+                <div className='logo'>
+                    navinda
+                </div>
+                <div className='imageOne'>
+                    <img
+                        alt='profile-image'
+                        src={imageOne}
+                        width="300px"
+                        height="400px"
+                    />
+                </div>
+                <div className='name'>
+                    <p>NAVINDA <br /> &nbsp; HEWAWICKRAMA</p>
+                </div>
+                <div className='leftText1'>
+                    Hello ! I am an undergraduate at the University of Ruhuna and an Associate Software Engineer.
+                </div>
 
-    </div>
-)
+                <div className='modeIcon'>
+                    <img src={modeIcon} alt="dark-light-mode-icon" />
+                    {/* turn the color red of the above icon */}
+                </div>
+            </div>
+
+
+            {/* red right side */}
+            <div className='rightColumn'>
+
+                <div className='toggleSwitch'>
+                    <label className="switch">
+                        <input type="checkbox" />
+                        <span className="slider round"></span>
+                        <p>Theme</p>
+                    </label>
+                </div>
+
+                <div className='alias'>
+                    <p>Alias</p>
+                    <p>Hermes</p>
+                </div>
+            </div>
+        </div>
+    )
 
 }
 
